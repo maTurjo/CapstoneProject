903590e52a994b8989184dc4f2ac1ea5b80b4786
Mubassir Ahmed Turjo <ma.turjo@gmail.com>
2022-03-22 15:42:04 -0400

    Featured Image Slider

---
+++ Capstone_git/Capstone_git/FeaturedCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  FeaturedCollectionViewCell.swift
+//  Capstone_git
+//
+//  Created by user202461 on 3/22/22.
+//
+
+import UIKit
+
+class FeaturedCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var FeaturedImage: UIImageView!
+}
--- Capstone_git/Capstone_git/ViewController.swift
+++ Capstone_git/Capstone_git/ViewController.swift
@@ -7,8 +7,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return featuredImages.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        
+        let cell=collectionView.dequeueReusableCell(withReuseIdentifier: "singleCell", for: indexPath)
+        as! FeaturedCollectionViewCell
+        
+        cell.FeaturedImage.image=UIImage(named: featuredImages[indexPath.row])
+        
+        return cell
+        
+    }
+    
 
+    
+    var featuredImages:[String]=["01","02","03","04","05"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
