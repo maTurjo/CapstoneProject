1ae779f580292d3607ad0dfb9112709a7a27259e
Mubassir Ahmed Turjo <ma.turjo@gmail.com>
2022-03-31 17:12:11 -0400

    Login Functionality Done

--- Capstone_git/Capstone_git/Controllers/LoginViewController.swift
+++ Capstone_git/Capstone_git/Controllers/LoginViewController.swift
@@ -7,23 +7,76 @@
 
 import UIKit
 
+struct loginData:Identifiable,Decodable{
+    var id=UUID()
+    let userName:String
+    let userPassword:String
+    
+    private enum CodingKeys:CodingKey{
+        case userName
+        case userPassword
+    }
+}
+
 class LoginViewController: UIViewController {
 
+   
+    
+    private var loginDataCol = [loginData]()
+    
+    @IBOutlet weak var errorBox: UITextField!
+    @IBOutlet weak var userName: UITextField!
+    @IBOutlet weak var userPassword: UITextField!
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-
+        loadLogins()
+        
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func loginClick(_ sender: Any) {
+        
+      
+        for login in loginDataCol {
+            errorBox.text=""
+            if(userName.text!.isEqual(login.userName) && userPassword.text!.isEqual(login.userPassword)){
+                changeToHomeScreen()
+                return
+            }
+            userName.text=""
+            userPassword.text=""
+            errorBox.text = "Please Enter Correct Credentials"
+            
+        }
+        errorBox.textColor=UIColor.red
+ 
+        
     }
-    */
+    
+    func changeToHomeScreen(){
+        let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let newViewController = storyBoard.instantiateViewController(withIdentifier: "homeScreen")
+                self.present(newViewController, animated: true, completion: nil)
+    }
+    
+    //Loading saved login data
+    func loadLogins(){
+        
+        guard let path=Bundle.main.path(forResource: "loginData", ofType: "json")else{
+            fatalError("File Location not found")
+            
+        }
+        
+        guard let data = try? Data(contentsOf: URL(fileURLWithPath: path)) else {return}
+        
+        let logindata = try? JSONDecoder().decode([loginData].self,from: data)
+        if let logindata=logindata{
+            self.loginDataCol=logindata
+        }
+        
+    }
 
 }
