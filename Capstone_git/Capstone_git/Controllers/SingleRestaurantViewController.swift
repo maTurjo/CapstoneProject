9a5262c9b7e41ef5b42626ccec60e43b24d0124e
Mihir Bhoi <bhoi.mihir7@gmail.com>
2022-04-14 03:24:12 -0400

    Add to Cart Button

--- Capstone_git/Capstone_git/Controllers/SingleRestaurantViewController.swift
+++ Capstone_git/Capstone_git/Controllers/SingleRestaurantViewController.swift
@@ -39,11 +39,47 @@ class SingleRestaurantViewController: UIViewController,UITableViewDelegate,UITab
             cell.FoodImage.image = UIImage(named: foodItem.foodItemImage[indexPath.row])
             //cell.FoodName.text = foodItem.foodItemName[indexPath.row]
             cell.FoodPrice.text = foodItem.foodItemPrice[indexPath.row]
-            
+            cell.addToCart.tag = indexPath.row
+            cell.addToCart.addTarget(self, action: #selector(subscribeTapped(_:)), for: .touchUpInside)
             //print(".\(menu.count) result ")
             return cell
         }
-        
+     
+    @objc func subscribeTapped(_ sender: UIButton){
+      // use the tag of button as index
+        print(sender.tag)
+        if(sender.tag == 0){
+            foodItem.foodItemPrice.append("CAD 12.00");
+            foodItem.foodItemName.append("Pizza");
+            foodItem.foodItemImage.append("pizza.png");
+        }
+        if(sender.tag == 1)
+        {
+            foodItem.foodItemPrice.append("CAD 15.00");
+            foodItem.foodItemName.append("Wings");
+            foodItem.foodItemImage.append("wings.png");
+        }
+        if(sender.tag == 2)
+        {
+            foodItem.foodItemPrice.append("CAD 7.00");
+            foodItem.foodItemName.append("Burger");
+            foodItem.foodItemImage.append("burger.png");
+        }
+        if(sender.tag == 3)
+        {
+            foodItem.foodItemPrice.append("CAD 9.00");
+            foodItem.foodItemName.append("Pasta");
+            foodItem.foodItemImage.append("pasta.png");
+        }
+        if(sender.tag == 4)
+        {
+            foodItem.foodItemPrice.append("CAD 6.00");
+            foodItem.foodItemName.append("Fries");
+            foodItem.foodItemImage.append("fries.png");
+        }
+        //CartViewController.TableViewCartItem.reloadData()
+    }
+    
     }
     
 
@@ -79,6 +115,7 @@ class FoodTableCell : UITableViewCell{
     @IBOutlet weak var FoodName: UILabel!
     @IBOutlet weak var FoodPrice: UILabel!
     
+    @IBOutlet weak var addToCart: UIButton!
     @IBAction func AddToCart(_ sender: UIButton) {
     }
 }
@@ -86,7 +123,7 @@ class FoodTableCell : UITableViewCell{
 
 class foodItem
 {
-    static var foodItemName:[String]=["Pizza","wings","Burger","pasta","fries"]
+    static var foodItemName:[String]=["Pizza","Wings","Burger","Pasta","Fries"]
     static var foodItemPrice:[String]=["CAD 12.00","CAD 15.00","CAD 7.00","CAD 9.00","CAD 6.00"]
     static var foodItemImage:[String]=["pizza.png","wings.png","burger.png","pasta.png","fries.png"]
  
